faa714e9ac184ae3c76dbf31fd99f885fc0a81be
Alexei <>
2024-04-18 12:43:21 +0300

    3.1 puhelinluettelon backend step1

--- index.js
+++ index.js
@@ -37,45 +37,48 @@ app.get('/api/persons', (request, response) => {
 
 const generateId = () => {
     const maxId = persons.length > 0
-        ? Math.max(...persons.map(n => n.id))
-        : 0
-    return maxId + 1
+        ? Math.max(...persons.map(person => Number(person.id)))
+        : 0;
+    return maxId + 1;
 }
 
+
 app.post('/api/persons', (request, response) => {
-    const body = request.body
+    const body = request.body;
 
-    if (!body.content) {
+    if (!body.name || !body.number) {
         return response.status(400).json({
-            error: 'content missing'
-        })
+            error: 'name or number missing'
+        });
     }
 
-    const note = {
-        content: body.content,
-        important: body.important || false,
+    const person = {
+        name: body.name,
+        number: body.number,
         id: generateId(),
-    }
+    };
 
-    persons = persons.concat(note)
+    persons = persons.concat(person);
 
-    response.json(note)
-})
+    response.json(person);
+});
+
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = persons.find(note => note.id === id)
-    if (note) {
-        response.json(note)
+    const id = request.params.id;
+    const person = persons.find(person => person.id === id);
+    if (person) {
+        response.json(person);
     } else {
-        console.log('x')
-        response.status(404).end()
+        console.log('x');
+        response.status(404).end();
     }
-})
+});
+
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
-    persons = persons.filter(note => note.id !== id)
+    persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
 })
